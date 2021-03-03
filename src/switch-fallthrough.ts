890db32fa6f9a24fa4d74436a08a18c2dfb5b084
ljsoo123 <ljsoo123456@naver.com>
2021-03-03 19:45:12 +0900

    switch-case fallthrough

---
+++ src/switch-fallthrough.ts
@@ -0,0 +1,17 @@
+let input = 2;
+switch (input % 2) {
+  case 0:
+    console.log("숫자 0"); //fall-through 발생
+  case 1:
+    console.log("숫자 1");
+    break;
+}
+
+//if noFallthroughCasesInSwitch is true
+let input2 = 0;
+switch (input2) {
+  case 0: //case절에 명령문을 넣지 않는다.
+  case 1:
+    console.log("숫자 1");
+    break;
+}
