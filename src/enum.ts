6105bb6cf97b18b1176ffc7b24366ed3dbda3763
ljsoo123 <ljsoo123456@naver.com>
2021-03-03 12:10:08 +0900

    [MODIFY] 오타 수정

--- src/enum.ts
+++ src/enum.ts
@@ -19,7 +19,7 @@ enum WeekDay3 {
 console.log("1. " + JSON.stringify(WeekDay));
 console.log("2. " + JSON.stringify(WeekDay2));
 console.log("3. " + JSON.stringify(WeekDay3));
-console.log(`4. Mon=${WeekDay.Mon}, Tue=${WeekDay["Tue"]}, We${WeekDay.Wed}`);
+console.log(`4. Mon=${WeekDay.Mon}, Tue=${WeekDay["Tue"]}, Wed=${WeekDay.Wed}`);
 console.log(`5. 0=${WeekDay[0]} 1=${WeekDay["1"]} 3=${WeekDay[WeekDay.Wed]}`);
 console.log(`6. `, typeof WeekDay.Mon, typeof WeekDay3.Mon);
 console.log("7. ", typeof WeekDay[0], WeekDay[0]);
