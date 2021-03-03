0cdcd41c4d32f5a9e24151754ee3b1eb0719ef9a
ljsoo123 <ljsoo123456@naver.com>
2021-03-03 19:00:15 +0900

    void null undefined

---
+++ dist/void-undefined-null.js
@@ -0,0 +1,6 @@
+var testUndefined;
+console.log(testUndefined, typeof testUndefined);
+var testUndefined2 = undefined;
+console.log(testUndefined2, typeof testUndefined2);
+var testNull = null;
+console.log(testNull, typeof testNull);
---
+++ src/void-undefined-null.ts
@@ -0,0 +1,8 @@
+var testUndefined;
+console.log(testUndefined, typeof testUndefined);
+
+var testUndefined2: undefined = undefined;
+console.log(testUndefined2, typeof testUndefined2);
+
+var testNull: null = null;
+console.log(testNull, typeof testNull);
