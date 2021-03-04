08b623cacb71cc21d4819201efa4aadfba2503f3
ljsoo123 <ljsoo123456@naver.com>
2021-03-04 13:48:57 +0900

    for문

---
+++ dist/for.js
@@ -0,0 +1,9 @@
+//es5
+for (var i = 0; i < 2; i++) {
+    console.log(`i = ${i}`);
+}
+console.log(`i2 = ${i}`);
+//typescript
+for (let j = 0; j < 2; j++) {
+    console.log(`j = ${j}`);
+}
---
+++ src/for.ts
@@ -0,0 +1,10 @@
+//es5
+for (var i = 0; i < 2; i++) {
+  console.log(`i = ${i}`);
+}
+console.log(`i2 = ${i}`);
+
+//typescript
+for (let j: number = 0; j < 2; j++) {
+  console.log(`j = ${j}`);
+}
