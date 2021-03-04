3b9995dca47350dc45701f791b3710048cee6fc2
ljsoo123 <ljsoo123456@naver.com>
2021-03-04 16:52:30 +0900

    while

---
+++ dist/while.js
@@ -0,0 +1,7 @@
+let count = 1;
+let sum = 0;
+while (count <= 100) {
+    sum += count;
+    count++;
+}
+console.log(sum);
---
+++ src/while.ts
@@ -0,0 +1,8 @@
+let count: number = 1;
+let sum: number = 0;
+
+while (count <= 100) {
+  sum += count;
+  count++;
+}
+console.log(sum);
