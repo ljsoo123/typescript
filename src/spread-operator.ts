b99a782ab6d80ee83648a126dcbfb5c128e849df
ljsoo123 <ljsoo123456@naver.com>
2021-03-05 11:00:03 +0900

    스프레드 연산자

---
+++ src/spread-operator.ts
@@ -0,0 +1,8 @@
+export {};
+let arr: number[] = [1, 2];
+let arr2: number[] = [...arr, 3, 4];
+console.log("1번 : ", arr2);
+let [firstItem, ...rest]: [number, number, number] = [10, 20, 30];
+console.log("2번 : ", firstItem);
+console.log("3번 : ", rest);
+console.log("4번 : ", rest[0]);
