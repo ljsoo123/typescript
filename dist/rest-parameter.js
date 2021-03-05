91e0a1720e0bb100eb403d4738a67287aea4c43a
ljsoo123 <ljsoo123456@naver.com>
2021-03-05 20:11:15 +0900

    rest parameter

--- dist/rest-parameter.js
+++ dist/rest-parameter.js
@@ -3,4 +3,8 @@ function colors(a, ...rest) {
 }
 let color1 = colors("red");
 let color2 = colors("red", "orange");
+let color3 = colors("red", "orange", "yellow");
+console.log(`color1 = ${color1}
+color2 = ${color2}
+color3 = ${color3}`);
 export {};
--- src/rest-parameter.ts
+++ src/rest-parameter.ts
@@ -6,3 +6,8 @@ function colors(a: string, ...rest: string[]) {
 
 let color1 = colors("red");
 let color2 = colors("red", "orange");
+let color3 = colors("red", "orange", "yellow");
+
+console.log(`color1 = ${color1}
+color2 = ${color2}
+color3 = ${color3}`);
